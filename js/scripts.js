1a20754db0c0ba52855fa0102a823da8d78ea564
Juan Esparza R <jesparzarom@outlook.com>
2022-12-27 03:18:49 -0300

    Fix responsive and reorder content

--- js/scripts.js
+++ js/scripts.js
@@ -19,35 +19,18 @@ $(document).ready(function(){
 });    
 
 
+//-----------COLAPSAR (Sin utilizar) ----------------//
+var coll = document.getElementsByClassName("colapsado");
+var i;
 
-
-//-----------------------------SCROLL---------------------------//
-//-- Script visto de Antonio Almeida -> url: jsfiddle.net/user/promatik/fiddles/ --//
-
-/*  NO FUNCIONA CORRECTAMENTE EN TODOS LOS NAVEGADORES.
-
-
-if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
-window.onmousewheel = document.onmousewheel = wheel;
-
-function wheel(event) {
-    var delta = 0;
-    if (event.wheelDelta) delta = event.wheelDelta / 120;
-    else if (event.detail) delta = -event.detail / 3;
-
-    handle(delta);
-    if (event.preventDefault) event.preventDefault();
-    event.returnValue = false;
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
 }
-
-function handle(delta) {
-    var time = 900;
-    var distance = 500;
-
-    $('html, body').stop().animate({
-        scrollTop: $(window).scrollTop() - (distance * delta)
-    }, time );
-}
-
-*/
-//------------------------------------------------------------//
