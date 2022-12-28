355853bec88dc9679568e80756650336913f6bd8
Juan Esparza R <jesparzarom@outlook.com>
2022-12-28 04:05:58 -0300

    Update Page

--- js/scripts.js
+++ js/scripts.js
@@ -2,35 +2,51 @@
 //-----------Script boton "ir arriba"----------------//
 
 $(document).ready(function(){
-
-    $(".up").click(function(){
-        $("body, html").animate({
-            scrollTop: "0px"
-        }, 900);
-    });
-
-    $(window).scroll(function(){
-        if( $(this).scrollTop() > 0){
-            $(".up").slideDown(300);
-        } else {
-            $(".up").slideUp(300);
-        }        
-    });
-});    
-
-
-//-----------COLAPSAR (Sin utilizar) ----------------//
-var coll = document.getElementsByClassName("colapsado");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
+  $(".scroll-top-button").click(function(){
+      $("body, html").animate({
+          scrollTop: "0px"
+      }, 1000); /* Desplazamiento suave durante 1 segundo */
   });
+
+  $(window).scroll(function(){
+      if( $(this).scrollTop() > 0){
+          $(".scroll-top-button").fadeIn(300);
+      } else {
+          $(".scroll-top-button").fadeOut(300);
+      }        
+  });
+}); 
+
+
+
+
+//-----------------------------SCROLL---------------------------//
+//-- Script visto de Antonio Almeida -> url: jsfiddle.net/user/promatik/fiddles/ --//
+
+/*  NO FUNCIONA CORRECTAMENTE EN TODOS LOS NAVEGADORES.
+
+
+if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
+window.onmousewheel = document.onmousewheel = wheel;
+
+function wheel(event) {
+  var delta = 0;
+  if (event.wheelDelta) delta = event.wheelDelta / 120;
+  else if (event.detail) delta = -event.detail / 3;
+
+  handle(delta);
+  if (event.preventDefault) event.preventDefault();
+  event.returnValue = false;
 }
+
+function handle(delta) {
+  var time = 900;
+  var distance = 500;
+
+  $('html, body').stop().animate({
+      scrollTop: $(window).scrollTop() - (distance * delta)
+  }, time );
+}
+
+*/
+//------------------------------------------------------------//
